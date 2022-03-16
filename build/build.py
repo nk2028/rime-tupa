c18a4ef8b06400fb3a11074d231fcf5943b51183
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-03-16 14:39:41 +0800

    Update build script

--- build/build.py
+++ build/build.py
@@ -1,48 +1,42 @@
-kyonh2描述 = {}
-
 with open('cache/kyonh.txt') as f:
-    for line in f:
-        描述, 拼音 = line.rstrip('\n').split('\t')
-        kyonh2描述[拼音] = 描述
-
-描述2tshet = {}
+    kyonh2descr = dict(line.rstrip('\n').split('\t')[::-1] for line in f)
 
 with open('cache/tshet.txt') as f:
-    for line in f:
-        描述, 拼音 = line.rstrip('\n').split('\t')
-        描述2tshet[描述] = 拼音
+    descr2tshet = dict(line.rstrip('\n').split('\t') for line in f)
 
-g2 = open('cache/unhandled2.txt', 'w')
+def do(fin, fout, ferr):
+    # header
+    for line in fin:
+        line = line.rstrip('\n')
+
+        if line == '...':
+            print(line, file=fout)
+            break
+
+        line = line.replace('kyonh', 'tshet')
+        print(line, file=fout)
+
+    # data
+    for line in fin:
+        line = line.rstrip('\n')
+
+        if not line:
+            print(file=fout)
+            continue
+
+        word, romans, *extras = line.split('\t')
+
+        try:
+            romans = ' '.join(descr2tshet[kyonh2descr[roman]] for roman in romans.split(' '))
+            print(word, romans, *extras, sep='\t', file=fout)
+        except KeyError:
+            for c, roman in zip(word, romans.split(' ')):
+                if roman not in kyonh2descr:
+                    print(roman, c, file=ferr)
 
 
-def do(input_file, output_file):
-    with open(input_file) as f, open(output_file, 'w') as g:
-        for line in f:
-            if line == '...\n':
-                g.write(line)
-                break
-            line = line.replace('kyonh', 'tshet')
-            g.write(line)
-
-        for line in f:
-            line = line.rstrip('\n')
-
-            if not line:
-                g.write('\n')
-                continue
-
-            ch, 拼音們, *extra = line.split('\t')
-
-            try:
-                new拼音們 = ' '.join(描述2tshet[kyonh2描述[拼音]] for 拼音 in 拼音們.split(' '))
-                print(ch, new拼音們, *extra, file=g, sep='\t')
-            except KeyError:
-                for c, 拼音 in zip(ch, 拼音們.split(' ')):
-                    if 拼音 not in kyonh2描述:
-                        print(拼音, c, file=g2)
-
-
-do('../rime-kyonh/kyonh.dict.yaml', 'tshet.dict.yaml')
-do('../rime-kyonh/kyonh.words.dict.yaml', 'tshet.words.dict.yaml')
-
-g2.close()
+with open('cache/unhandled2.txt', 'w') as ferr:
+    with open('../rime-kyonh/kyonh.dict.yaml') as fin, open('tshet.dict.yaml', 'w') as fout:
+        do(fin, fout, ferr)
+    with open('../rime-kyonh/kyonh.words.dict.yaml') as fin, open('tshet.words.dict.yaml', 'w') as fout:
+        do(fin, fout, ferr)
